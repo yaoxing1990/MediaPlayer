4dcf3febdb0a1424134a14ebfdca7988a9ed61b7
Xing Yao <yaoxing1990@gmail.com>
2017-10-18 20:33:35 -0500

    2

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://blooming-ridge-67597.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', ['POST', 'GET']);
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
